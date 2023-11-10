5dac34e5bfabfb524d827c957f9b22f95d0fea3f
joshua-lehmann <23728611+joshua-lehmann@users.noreply.github.com>
2023-11-10 14:31:28 +0100

    Added basic Widgets for demo

---
+++ lib/flexbox.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class FlexBox extends StatelessWidget {
+  const FlexBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Flexbox!');
+  }
+}
---
+++ lib/layouwidgets.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class LayoutWidgets extends StatelessWidget {
+  const LayoutWidgets({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('LayoutWidgets!');
+  }
+}
