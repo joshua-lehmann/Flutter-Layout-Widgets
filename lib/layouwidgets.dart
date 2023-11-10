a93f1e5d5769c5bfee1c6a329a51fdeabe411199
joshua-lehmann <23728611+joshua-lehmann@users.noreply.github.com>
2023-11-10 15:00:20 +0100

    Added samples for layout widgets

--- lib/layouwidgets.dart
+++ lib/layouwidgets.dart
@@ -1,10 +1,80 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class LayoutWidgets extends StatelessWidget {
   const LayoutWidgets({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text('LayoutWidgets!');
+    return SizedBox(
+      height: 700,
+      width: 800,
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  const Text('Layout Widgets'),
+                  Stack(
+                    children: <Widget>[
+                      Container(
+                        width: 300,
+                        height: 300,
+                        color: Colors.red,
+                      ),
+                      Container(
+                        width: 250,
+                        height: 250,
+                        color: Colors.black,
+                      ),
+                      Container(
+                        height: 200,
+                        width: 200,
+                        color: Colors.purple,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Container(decoration: BoxDecoration(border: Border.all(color:Colors.black)), height: 100, child: ListView(children: [
+                    Container(
+                      height: 50,
+                      color: Colors.blue,
+                      child: const Text('ListView 1'),
+                    ),
+                    Container(
+                      height: 50,
+                      color: Colors.yellow,
+                      child: const Text('ListView 2'),
+                    ),
+                    Container(
+                      height: 50,
+                      color: Colors.orange,
+                      child: const Text('ListView 3'),
+                    ),
+                  ]),),
+                  const SizedBox(height: 20),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.red,
+                      child: const Text('Expanded 1'),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      color: Colors.green,
+                      child: const Text('Expanded 2'),
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -57,25 +57,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Column(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: const Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-           FlexBox(),
+            FlexBox(),
             SizedBox(height: 20.0),
             LayoutWidgets()
-
           ],
-        ),
-      ),
-    );
+        ));
   }
 }
