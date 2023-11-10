dd34f0843795b3965a624e6a365ab7c46af7581a
wanjabachmann <wanja.bachmann@hotmail.com>
2023-11-10 15:00:05 +0100

    addd row col and container example

--- lib/flexbox.dart
+++ lib/flexbox.dart
@@ -1,10 +1,61 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class FlexBox extends StatelessWidget {
   const FlexBox({super.key});
-
-  @override
+@override
   Widget build(BuildContext context) {
-    return const Text('Flexbox!');
+    return Container(
+      color: Colors.yellowAccent.shade100,
+      padding: EdgeInsets.all(20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                width: 50,
+                height: 50,
+                color: Colors.red,
+              ),
+              Container(
+                width: 50,
+                height: 50,
+                color: Colors.green,
+              ),
+              Container(
+                width: 50,
+                height: 50,
+                color: Colors.blue,
+              ),
+            ],
+          ),
+          SizedBox(height: 20), // Platz zwischen den Widgets
+
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                color: Colors.orange,
+                padding: EdgeInsets.all(8),
+                child: Text('Element 1'),
+              ),
+              Container(
+                color: Colors.yellow,
+                padding: EdgeInsets.all(8),
+                child: Text('Element 2'),
+              ),
+              Container(
+                color: Colors.teal,
+                padding: EdgeInsets.all(8),
+                child: Text('Element 3'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
